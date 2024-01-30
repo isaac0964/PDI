314cd5baa52ba5c12a04ea1922db541cffe72cd9
Isaac Gomez <angelisasc1968@gmail.com>
2024-01-30 13:21:11 -0600

    Terminada practica 2

--- Practica2/Practica2_eqEspecificacion_PDI_GomezCanales.py
+++ Practica2/Practica2_eqEspecificacion_PDI_GomezCanales.py
@@ -20,20 +20,52 @@ def acumulado(figure, img):
         plt.pause(0.001)
     return H
 
+# ------------------- Puntos extras: Dar color a una imagen -------------------
+def colorear(I1, I2):
+    """
+    Esta funcion convierte la imagen 1 a escala de grises y le da color usando la distribucion de la imagen 2
+    input: I1 imagen a colorear, I2 imagen de la que se obtendra la distribucion
+    """
+
+    # Primero convertimos de bgr a escala de grises
+    I1_gray = cv2.cvtColor(I1, cv2.COLOR_BGR2GRAY)
+    # Ahora de escala de grises repetimos los mismos valores para los tres canales (BGR)
+    I1_tresc = np.zeros((I1_gray.shape[0], I1_gray.shape[1], 3), "uint8")
+    I1_tresc[:, :, 0] = I1_gray
+    I1_tresc[:, :, 1] = I1_gray
+    I1_tresc[:, :, 2] = I1_gray
+    I1 = I1_tresc
+
+    H1 = acumulado(1, I1)
+    H2 = acumulado(2, I2)
+
+    [m, n, p] = I1.shape
+
+    I3 = np.zeros_like(I1)
+    # -------------------- Ecualizacion por Especificacion ----------------------------
+    for k in range(p):
+        for i in range(m):
+            for j in range(n):
+                z = 0
+                Sk = H1[I1[i, j, k], k]
+                while H2[z, k] - Sk < 0:
+                    z += 1
+                    if z == 256:
+                        H2[z, k] = Sk
+                I3[i, j, k] = z
+
+    I3 = I3.astype("uint8")
+    H3 = acumulado(3, I3)
+
+    cv2.imshow("Img grises", I1)
+    cv2.imshow("Img ref", I2)
+    cv2.imshow("Img color", I3)
+
+
+
 # Cambiar ruta y nombre de las imgs
-I2 = cv2.imread("../imgs/lena.png")  
-I1 = cv2.imread("../imgs/puente.jpg")
-
-# --------------------- Puntos extra: dar color a una img ---------------------------
-# Primero convertimos de bgr a escala de grises
-I1_gray = cv2.cvtColor(I1, cv2.COLOR_BGR2GRAY)
-# Ahora de escala de grises repetimos los mismos valores para los tres canales (BGR)
-I1_tresc = np.zeros((I1_gray.shape[0], I1_gray.shape[1], 3), "uint8")
-I1_tresc[:, :, 0] = I1_gray
-I1_tresc[:, :, 1] = I1_gray
-I1_tresc[:, :, 2] = I1_gray
-
-I1 = I1_tresc
+I1 = cv2.imread("../imgs/bosque.png")  
+I2 = cv2.imread("../imgs/puente.jpg")
 
 H1 = acumulado(1, I1)
 H2 = acumulado(2, I2)
@@ -56,8 +88,12 @@ for k in range(p):
 I3 = I3.astype("uint8")
 H3 = acumulado(3, I3)
 
-cv2.imshow("Montana", I1)
-cv2.imshow("Puente", I2)
+cv2.imshow("Img 1", I1)
+cv2.imshow("Img 2", I2)
 cv2.imshow("EqEspecificacion", I3)
+
+# --------------------- Puntos extra: dar color a una img ---------------------------
+# para usar el coloreo de imagenes descomente la siguiente linea
+#colorear(I1, I2)
 
 cv2.waitKey(0)
